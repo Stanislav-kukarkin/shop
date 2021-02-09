645628113b71ad8a0077707930e5720b201e2e2a
Stas Kukarkin <staskykarkin@gmail.com>
2021-02-09 11:12:20 +0600

    add to serve

--- src/app/admin/add-page/add-page.component.ts
+++ src/app/admin/add-page/add-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ProductService } from 'src/app/shared/product.service';
 
 @Component({
   selector: 'app-add-page',
@@ -12,7 +13,9 @@ export class AddPageComponent implements OnInit {
 
   form: FormGroup
 
-  constructor() { }
+  constructor(
+    private productServ: ProductService
+  ) { }
 
   ngOnInit(): void {
     this.form = new FormGroup({
@@ -35,8 +38,10 @@ export class AddPageComponent implements OnInit {
       photo: this.form.value.photo,
       info: this.form.value.info,
       price: this.form.value.price,
+      date: new Date()
     }
     console.log(this.form)
+    this.productServ.create(product).subscribe( res => console.log(res))
   }
 
 }
